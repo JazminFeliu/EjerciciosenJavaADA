5fb09847900a3e496e6743745083e16551ce90e5
JazminFeliu <jazminfeliu@gmail.com>
2021-01-16 14:27:34 -0300

    Subo ejercicio Verduleria clase 16/01/2021 Repo VectoresJava5

---
+++ VectoresJava5/src/com/company/Articulo.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Articulo {
+
+    private String tipo;
+    private String nombre;
+    private Integer cantidad;
+
+    public Articulo (String tipo, String nombre, Integer cantidad){
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public Integer getCantidad(){
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad){
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Articulo{" +
+                "tipo='" + tipo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", cantidad=" + cantidad +
+                '}';
+    }
+}
+
