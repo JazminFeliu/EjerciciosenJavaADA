d535b0080439867a3f2e212b844cb1eb48314d08
JazminFeliu <jazminfeliu@gmail.com>
2021-02-09 11:36:22 -0300

    Subo ejercicio date3 clase 28/01 con fechas

---
+++ date3/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LocalDate fecha = LocalDate.ofYearDay(2021, 231);
+        System.out.println(fecha);
+
+    }
+}
