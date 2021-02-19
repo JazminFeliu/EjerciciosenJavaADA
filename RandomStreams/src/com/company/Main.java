061f7f6e5acb93fb63c5dbeb1124d746211ac68e
JazminFeliu <jazminfeliu@gmail.com>
2021-02-18 22:02:18 -0300

    Subo ejercicio Nros. Random con IntStream clase 18/02/2021

---
+++ RandomStreams/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random aleatorio = new Random(System.currentTimeMillis()); //semilla: hora actual de mi sistema
+
+        //un nro enterio pseudoaleatorio entre 0 y 5 (el 6 no lo incluye)
+        int enteroRandom = aleatorio.nextInt(6);
+        System.out.println("Nro. random: "+enteroRandom);
+
+        //Ejemplo random con Stream
+        IntStream enterosAleatorios = aleatorio.ints(9,1,53);
+        enterosAleatorios.forEach(numero -> System.out.println("Numero: "+numero));
+    }
+}
