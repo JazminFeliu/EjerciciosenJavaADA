1ddd539ff4d7e25b52e7ec0ed5691a12389ebdd2
JazminFeliu <jazminfeliu@gmail.com>
2021-03-18 21:54:14 -0300

    Actualizo ejercicio con Base de Datos MySql clase 18/03/2021 para cargar por java datos a la base

--- JavaDB/src/com/company/Main.java
+++ JavaDB/src/com/company/Main.java
@@ -15,18 +15,21 @@ public class Main {
 		String dbURL = "jdbc:mysql://localhost:3306/jaz";
 		String usuarioBD = "root";
 		String passDB = "root";
+		String sql = "INSERT INTO empleados(nombre, apellido, legajo) VALUES ('jorge','brisuela','C546')";
 
 		String query = "SELECT legajo, nombre, apellido FROM empleados";
 
 		try {
 			conexion = DriverManager.getConnection(dbURL, usuarioBD, passDB);
 			st = conexion.createStatement();
+			st.execute(sql);
 			resultados = st.executeQuery(query);
 
 			while(resultados.next()){
 				String nombre = resultados.getString("nombre");
+				String apellido = resultados.getString("apellido");
 				String legajo = resultados.getString("legajo");
-				System.out.println(nombre + " (legajo: "+legajo+")");
+				System.out.println(nombre +" "+ apellido+" (legajo: "+legajo+")");
 			}
 		} catch (SQLException e) { e.printStackTrace();
 		} finally {
